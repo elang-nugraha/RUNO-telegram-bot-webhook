205b3b7a47d2b4e34f46fd3b14882c7bccd884a0
Elang <elangunonugraha11@gmail.com>
2025-09-13 21:14:38 +0700

    add webhook route and env

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return jsonify(message="Hello, Flask! 🚀")
+
+@app.route("/webhook")
+def webhook():
+    a = os.getenv("BOT_NAME")
+    return jsonify(message=a)
 
 if __name__ == '__main__':
     app.run(debug=True)
