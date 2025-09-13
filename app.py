00ff331d737f713cb7a41ac1bf7e7fc9bee209b4
Elang <elangunonugraha11@gmail.com>
2025-09-13 16:34:25 +0700

    change on app

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return jsonify(message="Hello, Flask! 🚀")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    app.run(debug=True)
