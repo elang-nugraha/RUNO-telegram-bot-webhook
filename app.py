bac62555f04458159ad1f127950fcfccef48303d
Elang <elangunonugraha11@gmail.com>
2025-09-17 03:15:29 +0700

    send message to admin feature (BOT)

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify
 import os
+import asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from telegram import Bot
+TOKEN = os.getenv("TELEGRAM_KEY")
+bot = Bot(TOKEN)
 
 app = Flask(__name__)
-
 @app.route("/")
 def home():
     return jsonify(message="Hello, Flask! 🚀")
@@ -12,6 +19,8 @@ def webhook():
     a = os.getenv("BOT_NAME")
     os.environ["BOT_NAME"] = "test"
     b = os.getenv("BOT_NAME")
+
+    return a+b
 
 # using manually constructed response (api post)
 @app.route("/v1/webhook/")
@@ -23,11 +32,16 @@ def sendMessageByRequest():
     return jsonify(message=a+b)
 
 # wil be used bot variable 
+def botSendingMessage(): 
+    bot.sendMessage(int(os.getenv("ADMIN")), f"Hellooo admin")
+
 @app.route("/v2/webhook/")
 def sendMessageByBot():
     a = os.getenv("BOT_NAME")
     os.environ["BOT_NAME"] = "this is using bot library response"
     b = os.getenv("BOT_NAME")
+
+    asyncio.run(botSendingMessage())
 
     return jsonify(message=a+b)
 
