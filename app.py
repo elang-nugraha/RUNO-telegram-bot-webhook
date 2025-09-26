cf4113f3e6951f8eaf299b5caae213e4b34a76d0
Elang <elangunonugraha11@gmail.com>
2025-09-26 15:16:25 +0700

    add command

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ TOKEN = os.getenv("TELEGRAM_KEY2")
 bot = Bot(TOKEN)
 app = Flask(__name__)
 
+loop = asyncio.new_event_loop()
+
 @app.route("/")
 def home():
     # will be used for static web(maybe)
@@ -30,31 +32,43 @@ def webhook():
         # access user database
         # access user Id state or env status (conv)
 
+    message = update.get("message").get("text")
+    message = message.strip()
+    botCommands = "BOT Commands\n\n \
+        /userRegistration \n " \
+        "/openRegistration \n " \
+        "/closeRegistration \n" \
+        "/addItem \n" \
+        "/updateStock \n" \
+        "/getStock \n" \
+        "/addTransaction"
+
+    if (message == "/start" ):
+        sendMessageByBot("Hello Welcome to ", user.getId())
+        sendMessageByBot(botCommands, user.getId())
+    else:
+        sendMessageByBot(botCommands, user.getId())
+        sendMessageByBot("Ask admin for use commands", user.getId())
     # message handling
         # command message
         # based on env status (conv)
 
     return {"ok": True}
 
+def sendMessageByBot(message  : str, id : int):
+    if loop.is_running():
+        loop.create_task(bot.send_message(bot.send_message(id, message)))
+    else:
+        loop.run_until_complete(bot.send_message(id, message))
+
 # using manually constructed response (api post)
-@app.route("/v1/webhook/")
-def sendMessageByRequest():
-    a = os.getenv("BOT_NAME")
-    os.environ["BOT_NAME"] = "this is manually response"
-    b = os.getenv("BOT_NAME")
+# @app.route("/v1/webhook/")
+# def sendMessageByRequest():
+#     a = os.getenv("BOT_NAME")
+#     os.environ["BOT_NAME"] = "this is manually response"
+#     b = os.getenv("BOT_NAME")
 
-    return jsonify(message=a+b)
-
-# wil be used bot variable 
-@app.route("/v2/webhook/")
-def sendMessageByBot():
-    a = os.getenv("BOT_NAME")
-    os.environ["BOT_NAME"] = "this is using bot library response"
-    b = os.getenv("BOT_NAME")
-
-    asyncio.run(bot.sendMessage(int(os.getenv("ADMIN")), f"Hellooo admin"))
-
-    return jsonify(message=a+b)
+#     return jsonify(message=a+b)
 
 if __name__ == "__main__":
     app.run(debug=True)
